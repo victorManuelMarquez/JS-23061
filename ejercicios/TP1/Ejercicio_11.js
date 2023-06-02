d5b8de69ee60ac4e13b295997df5649cb75d6aff
Víctor Manuel Márquez <107000454+victorManuelMarquez@users.noreply.github.com>
2023-06-02 15:30:12 -0300

    Fix mistake in Ejercicio_11.js

--- ejercicios/TP1/Ejercicio_11.js
+++ ejercicios/TP1/Ejercicio_11.js
@@ -2,5 +2,4 @@
 let base = prompt("Ingrese el valor de la base:");
 let altura = prompt("Ingrese la altura:");
 console.log('superficie: ' + (base * altura / 2).toFixed(1) + '&sup2;');
-let per = Math.pow(base/2, 2) + Math.pow(altura, 2);
-console.log('perímetro: ' + (parseFloat(base) + Math.sqrt(per) * 2).toFixed(2));
+console.log('perímetro: ' + (base*3).toFixed(2));
